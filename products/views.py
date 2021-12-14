29ded45a30b0305695dd0e4a807624bde45ef89f
Kevin <kevinoc554@gmail.com>
2021-12-14 19:33:29 +0000

    feat: create products view, update app and project urls, and start product template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+    """
+    A view to return all products, and render them on the Catalog page
+    """
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products/product.html', context)

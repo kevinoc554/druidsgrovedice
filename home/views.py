009431fab80fa2bba7054804aba7cc09a5b85602
Kevin <kevinoc554@gmail.com>
2021-12-17 18:08:06 +0000

    feat: update index template and home route to display the 4 most recently added products on the home page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from products.models import Product
 
 
 def index(request):
     """
-    A view to return the Index page
+    A view to return the Index page,
+    and show the four most recently added products
     """
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+    length = len(products)
+    products = products[length-4:length]
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'home/index.html', context)

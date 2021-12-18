85d4c5056a8f42ce1ce5124c5517b2861f497840
Kevin <kevinoc554@gmail.com>
2021-12-18 16:34:40 +0000

    feat: add return to top button, styling and fucntionality to base template

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
     autoFocusModal();
     sortSelector();
+    toTopBtn();
 })
 
+// Autofocus inputs on search modal
 function autoFocusModal() {
     $('#searchModal').on('shown.bs.modal', function () {
         $('#product_search').trigger('focus')
     })
 }
 
+// Re-sort the Catalog page when select element is changed
 function sortSelector() {
     $('#sort-selector').change(function () {
         let selector = $(this)
@@ -29,4 +32,25 @@ function sortSelector() {
             window.location.replace(currentUrl)
         }
     })
+}
+
+// Fade in Return to Top Button, smooth scroll to top on click, allow user to interrupt scroll with input
+function toTopBtn() {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('#toTopBtn').fadeIn();
+        } else {
+            $('#toTopBtn').fadeOut();
+        }
+    });
+    $('#toTopBtn').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    $('html, body').on('scroll mousedown DOMMouseScroll mousewheel keyup touchstart', function (e) {
+        if (e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel' || e.type === 'touchstart') {
+            $('html, body').stop();
+        }
+    });
 }

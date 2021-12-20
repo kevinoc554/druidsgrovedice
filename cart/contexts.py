429ba7d3c8e0bd76ea397e9e3943234612eb06b5
Kevin <kevinoc554@gmail.com>
2021-12-20 17:09:47 +0000

    feat: create context processor for shopping cart items

---
+++ cart/contexts.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from decimal import Decimal
+
+
+def cart_contents(request):
+    cart_items = []
+    total = 0
+    product_count = 0
+    if total < settings.FREE_DELIVERY_THREHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THREHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = total + delivery
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THREHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context

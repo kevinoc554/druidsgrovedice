aec715f8c23ac534e442af7c35c2691cb4988b5a
Kevin <kevinoc554@gmail.com>
2021-12-26 18:52:13 +0000

    feat: create order form and render to checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    A view to render the checkout form
+    """
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, 'The shopping cart is empty')
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, 'checkout/checkout.html', context)

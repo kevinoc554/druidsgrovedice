3d7bab441697e48e5a787ba32856531e8872efb4
Kevin <kevinoc554@gmail.com>
2021-12-28 19:59:31 +0000

    feat: add logic to crete stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -12,20 +12,28 @@ def checkout(request):
     """
     A view to render the checkout form
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'The shopping cart is empty')
         return redirect(reverse('products'))
-    
+
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JltkWJQL8Nbmp5ECJpkJLRTJ3auzBnKA5X8AbbzTlUJeDhciSq2OkFKXEo3OibZRGbbCag7kDzfNO20f4KYqV2500pblWjE8s',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
 
     return render(request, 'checkout/checkout.html', context)

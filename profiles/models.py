08ff7666a94bae01c9826210df4b5df6d94995be
Kevin <kevinoc554@gmail.com>
2022-01-05 17:53:07 +0000

    feat: update profile model and view to pass user profile to template

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     """
     A User profile model for tracking default delivery info and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20,
                                             null=True, blank=True)
     default_country = CountryField(blank_label="Country*",
@@ -28,7 +28,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
     @receiver(post_save, sender=User)
     def update_or_create_user_profile(sender, instance, created, **kwargs):
         """
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """
     A view to return the user's profile page
     """
+    userprofile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': userprofile,
+    }
 
     return render(request, template, context)

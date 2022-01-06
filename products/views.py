312f42edb89f2ec01742e4fe51731e5ffbb333d8
Kevin <kevinoc554@gmail.com>
2022-01-06 21:45:11 +0000

    feat: hook up and style add product link, redirect add product few to show newly added product details

--- products/views.py
+++ products/views.py
@@ -74,9 +74,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product successfully added.')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, (
                 'Failed to add product. Please check your form is correct'

fdc0f64fd434d818aba133f30efc1665be302677
Kevin <kevinoc554@gmail.com>
2022-01-19 13:19:12 +0000

    feat: create blogpost app and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title

398f2b426843ffcdc8912f0e09ba651703c6c150
Kevin <kevinoc554@gmail.com>
2022-01-19 14:28:09 +0000

    feat: update blogpost model, make migrations and add test post

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 
 
 class BlogPost(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=200, null=True, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True)
 
     class Meta:
         ordering = ['-created_on']

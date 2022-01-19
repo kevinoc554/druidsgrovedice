b0c601c073d1dcf94a95d46b659f68d4087fc311
Kevin <kevinoc554@gmail.com>
2022-01-19 17:30:04 +0000

    feat: pass blog posts through the view to the template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 
 def all_blogposts(request):
@@ -6,6 +7,12 @@ def all_blogposts(request):
     A view to return all blog posts,
     and render them on the Blog page
     """
+    first = BlogPost.objects.first()
+    blogposts = BlogPost.objects.all()[1:]
     template = 'blog/blog.html'
+    context = {
+        'first': first,
+        'blogposts': blogposts,
+    }
 
-    return render(request, template)
+    return render(request, template, context)
